0d1f803893d0d620dfc11df067c59bf7799d0867
fatimaaziz <bsef16m522@pucit.edu.pk>
2018-05-02 17:58:50 +0500

    shortesst job first

--- sjf.py
+++ sjf.py
@@ -1,5 +1,5 @@
 
-print "shortest job first\n"
+print ("shortest job first\n")
 arival_time=[0,3,2]
 process=['p1','p2','p3']
 burst_time=[13,6,3]
@@ -21,16 +21,26 @@ for i in range(2):
                 process[j]=process[j+1]
                 process[j+1]=temp2
      time=burst_time[i]+time
-    
+
+time=0
+print("gang chart")
+for index in range(3):
+     print (time,':',process[index],':',time+burst_time[index])
+     time=time+burst_time[index]    
 
 for index in range(3):
-     print process[index]
+     print (process[index])
      
      finishtime=finishtime+burst_time[index]
      wait_time[index]=start_time-arival_time[index]
   
-     print 'time for wating',wait_time[index]
+     print ('time for wating',wait_time[index])
      turnaround[index]=finishtime-arival_time[index]
     
-     print 'turnaroundtime',turnaround[index]
+     print ('turnaroundtime',turnaround[index])
      start_time=start_time+burst_time[index]
+
+
+    
+	 
+
