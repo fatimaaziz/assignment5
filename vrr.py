eb62d62f5eda7cd6dda029905e33a1eefbd0fe91
fatimaaziz <bsef16m522@pucit.edu.pk>
2018-05-04 22:48:02 +0500

    virtual round robin

--- vrr.py
+++ vrr.py
@@ -1,6 +1,7 @@
 print("round robbin with input output")
 ready=[0]*100#making two queues
 readyrear=0
+readyfront=0
 waitingqueue=[0]*50
 waitingrear=0
 waitingfront=0
@@ -29,7 +30,7 @@ for i in range (n):
 		
 for i in range(n-1):#sorting acorrding to arriva time
         for j in range(n-1-i):
-				if(ready[j+1]['arrival']<ready[j]['arrival'])
+				if(ready[j+1]['arrival']<ready[j]['arrival']):
 					temp=ready[j]
 					ready[j]=ready[j+1]
 					ready[j+1]=ready[j]
@@ -38,10 +39,9 @@ time=ready[0]['arrival']
 resultant=[0]*n
 rp=n
 while(rp!=0):
-		if(ready[i]['pro is what']==1)#if odd proces
+		if(ready[i]['pro is what']==1):#if odd proces
 		       if(ready[readyfront]['remaining time']>qt):
                   time=time+qt
-                  ready[readyfront]['remaining time'] =ready[readyfront]['remaining time']-timeslice
                   print (ready[readyfront]['proname'],':',time)
 				  ready[readyfront]['remaining time']=ready[readyfront]['remaining time']-qt
 				  ready[readyrear]=ready[readyfront]
@@ -84,9 +84,8 @@ while(rp!=0):
 		    axiliaryqueue[axxrear]=waitingqueue[waitingfront]
 			if(axiliaryqueue[axxfront]['remaining time']>axiliaryqueue[axxfront]['remaining qt']):
                     time=time+axiliaryqueue[axxfront]['remaining qt']
-					axiliaryqueue[axxfront]['remaining time'] =ready[readyfront]['remaining time']-timeslice
 					print (axiliaryqueue[axxfront]['proname'],':',time)
-					axiliaryqueue[axxfront]['remaining time']=ready[readyfront]['remaining time']-qt
+					axiliaryqueue[axxfront]['remaining time']=axiliaryqueue[axxfront]['remaining qt']-qt
 					axiliaryqueue[axxfront]['waitingqueuert']=0
 					axiliaryqueue[axxfront]['remaining qt']=0
 					
@@ -94,14 +93,24 @@ while(rp!=0):
 					readyrear=readyrear+1
 				  
 				  
-               elif(axiliaryqueue[axxfront]['remaining time']<=axiliaryqueue[axxfront]['remaining qt']) and axiliaryqueue[axxfront]['remaining time']>0):
+               elif(axiliaryqueue[axxfront]['remaining time']<=axiliaryqueue[axxfront]['remaining qt'] and axiliaryqueue[axxfront]['remaining time']>0):
                   time=time+axiliaryqueue[axxfront]['remaining time']
                   axiliaryqueue[axxfront]['remaining time']=0
                   print (axiliaryqueue[axxfront],':',time)
                   axiliaryqueue[axxfront]['finish time']=time
                   axiliaryqueue[axxfront]['turnaround']=axiliaryqueue[axxfront]['finish time']-axiliaryqueue[axxfront]['arrival']
                   axiliaryqueue[axxfront]['waiting']=axiliaryqueue[axxfront]['finish time']-axiliaryqueue[axxfront]['arrival']-axiliaryqueue[axxfront]['brust']
-                  rp=rp-1
+                  resultant[rp-1]=axiliaryqueue[axxfront]
+				  rp=rp-1
 		
 			waitingfront=waitingfront+1
-		
+		turnarround=0
+wait=0
+for i in range(n):
+	 turnarround=resultant[rp]['turnaround']+turnarround 
+	 wait=wait+resultant[rp]['waiting']
+
+wait=wait/n
+turnaround=turnaround/n
+print("average trunouttime",turnarround)
+print("average waitingtime",wait)
