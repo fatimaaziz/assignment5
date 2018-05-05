a89fd39275b89720a5854419c8d2d27114dfceb3
fatimaaziz <bsef16m522@pucit.edu.pk>
2018-05-05 13:15:07 +0500

    virtual round robin

--- vrr.py
+++ vrr.py
@@ -30,22 +30,23 @@ for i in range (n):
 		
 for i in range(n-1):#sorting acorrding to arriva time
         for j in range(n-1-i):
-				if(ready[j+1]['arrival']<ready[j]['arrival']):
-					temp=ready[j]
-					ready[j]=ready[j+1]
-					ready[j+1]=ready[j]
+                if(ready[j+1]['arrival']<ready[j]['arrival']):
+                    temp=ready[j]
+                    ready[j]=ready[j+1]
+                    ready[j+1]=ready[j]
  
 time=ready[0]['arrival']
 resultant=[0]*n
 rp=n
 while(rp!=0):
-		if(ready[i]['pro is what']==1):#if odd proces
-		       if(ready[readyfront]['remaining time']>qt):
+    if(readyfront<readyrear):
+        if(ready[i]['pro is what']==1):#if odd proces
+               if(ready[readyfront]['remaining time']>qt):
                   time=time+qt
                   print (ready[readyfront]['proname'],':',time)
-				  ready[readyfront]['remaining time']=ready[readyfront]['remaining time']-qt
-				  ready[readyrear]=ready[readyfront]
-				  readyrear=readyrear+1
+                  ready[readyfront]['remaining time']=ready[readyfront]['remaining time']-qt
+                  ready[readyrear]=ready[readyfront]
+                  readyrear=readyrear+1
                elif(ready[readyfront]['remaining time']<=qt and ready[readyfront]['remaining time']>0):
                   time=time+ready[readyfront]['remaining time']
                   ready[readyfront]['remaining time']=0
@@ -54,21 +55,21 @@ while(rp!=0):
                   ready[readyfront]['turnaround']=ready[readyfront]['finish time']-ready[readyfront]['arrival']
                   ready[readyfront]['waiting']=ready[readyfront]['finish time']-ready[readyfront]['arrival']-ready[readyfront]['brust']
                   resultant[rp]=ready[readyfront]
-				  rp=rp-1
-		   readyfront=readyfront+1
-		elif(ready[i]['pro is what']==0)
-		     if(ready[readyfront]['remaining time']>qt1):
+                  rp=rp-1
+               readyfront=readyfront+1
+        elif(ready[i]['pro is what']==0):
+             if(ready[readyfront]['remaining time']>qt1):
                   time=time+qt1
                   ready[readyfront]['remaining time'] =ready[readyfront]['remaining time']-timeslice
                   print (ready[readyfront]['proname'],':',time)
-				  ready[readyfront]['remaining time']=ready[readyfront]['remaining time']-qt
-				  ready[readyfront]['waitingqueuert']=time+inp
-				  ready[readyfront]['remaining qt']=qt-qt1
-				  waitingqueue[waitingrear]=ready[readyfront]
-				  waitingrear=waitingrear+1
-				  q1=0
-				  
-               elif(ready[readyfront]['remaining time']<=qt1 and ready[readyfront]['remaining time']>0):
+                  ready[readyfront]['remaining time']=ready[readyfront]['remaining time']-qt
+                  ready[readyfront]['waitingqueuert']=time+inp
+                  ready[readyfront]['remaining qt']=qt-qt1
+                  waitingqueue[waitingrear]=ready[readyfront]
+                  waitingrear=waitingrear+1
+                  q1=0
+                  
+             elif(ready[readyfront]['remaining time']<=qt1 and ready[readyfront]['remaining time']>0):
                   time=time+ready[readyfront]['remaining time']
                   ready[readyfront]['remaining time']=0
                   print (ready[readyfront]['proname'],':',time)
@@ -76,35 +77,35 @@ while(rp!=0):
                   ready[readyfront]['turnaround']=ready[readyfront]['finish time']-ready[readyfront]['arrival']
                   ready[readyfront]['turnaround']=ready[readyfront]['finish time']-ready[readyfront]['arrival']-ready[readyfront]['brust']
                   rp=rp-1
-			readyfront=readyfront+1	  
-			  
-        	
-        if(waitingqueue[waitingfront]['waitingqueuert']<=time)
-		    ready[readyfront]['waitingqueuert']=0
-		    axiliaryqueue[axxrear]=waitingqueue[waitingfront]
-			if(axiliaryqueue[axxfront]['remaining time']>axiliaryqueue[axxfront]['remaining qt']):
+             readyfront=readyfront+1   
+              
+    elif(waitingfront<waitingrear):        
+        if(waitingqueue[waitingfront]['waitingqueuert']<=time):
+            ready[readyfront]['waitingqueuert']=0
+            axiliaryqueue[axxrear]=waitingqueue[waitingfront]
+            if(axiliaryqueue[axxfront]['remaining time']>axiliaryqueue[axxfront]['remaining qt']):
                     time=time+axiliaryqueue[axxfront]['remaining qt']
-					print (axiliaryqueue[axxfront]['proname'],':',time)
-					axiliaryqueue[axxfront]['remaining time']=axiliaryqueue[axxfront]['remaining qt']-qt
-					axiliaryqueue[axxfront]['waitingqueuert']=0
-					axiliaryqueue[axxfront]['remaining qt']=0
-					
-					ready[readyrear]=axiliaryqueue[axxfront]
-					readyrear=readyrear+1
-				  
-				  
-               elif(axiliaryqueue[axxfront]['remaining time']<=axiliaryqueue[axxfront]['remaining qt'] and axiliaryqueue[axxfront]['remaining time']>0):
-                  time=time+axiliaryqueue[axxfront]['remaining time']
-                  axiliaryqueue[axxfront]['remaining time']=0
-                  print (axiliaryqueue[axxfront],':',time)
-                  axiliaryqueue[axxfront]['finish time']=time
-                  axiliaryqueue[axxfront]['turnaround']=axiliaryqueue[axxfront]['finish time']-axiliaryqueue[axxfront]['arrival']
-                  axiliaryqueue[axxfront]['waiting']=axiliaryqueue[axxfront]['finish time']-axiliaryqueue[axxfront]['arrival']-axiliaryqueue[axxfront]['brust']
-                  resultant[rp-1]=axiliaryqueue[axxfront]
-				  rp=rp-1
-		
-			waitingfront=waitingfront+1
-		turnarround=0
+                    print (axiliaryqueue[axxfront]['proname'],':',time)
+                    axiliaryqueue[axxfront]['remaining time']=axiliaryqueue[axxfront]['remaining qt']-qt
+                    axiliaryqueue[axxfront]['waitingqueuert']=0
+                    axiliaryqueue[axxfront]['remaining qt']=0
+                    
+                    ready[readyrear]=axiliaryqueue[axxfront]
+                    readyrear=readyrear+1
+                  
+                  
+            elif(axiliaryqueue[axxfront]['remaining time']<=axiliaryqueue[axxfront]['remaining qt'] and axiliaryqueue[axxfront]['remaining time']>0):
+                    time=time+axiliaryqueue[axxfront]['remaining time']
+                    axiliaryqueue[axxfront]['remaining time']=0
+                    print (axiliaryqueue[axxfront],':',time)
+                    axiliaryqueue[axxfront]['finish time']=time
+                    axiliaryqueue[axxfront]['turnaround']=axiliaryqueue[axxfront]['finish time']-axiliaryqueue[axxfront]['arrival']
+                    axiliaryqueue[axxfront]['waiting']=axiliaryqueue[axxfront]['finish time']-axiliaryqueue[axxfront]['arrival']-axiliaryqueue[axxfront]['brust']
+                    resultant[rp-1]=axiliaryqueue[axxfront]
+                    rp=rp-1
+        
+            waitingfront=waitingfront+1
+turnarround=0
 wait=0
 for i in range(n):
 	 turnarround=resultant[rp]['turnaround']+turnarround 
